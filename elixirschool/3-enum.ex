dc7a7a79515725b0f81d01dc84f102c789d4981a
jdruk <devjosafa@gmail.com>
2019-05-09 22:09:59 -0300

    primeira parte enum elixirschool

---
+++ elixirschool/3-enum.ex
@@ -0,0 +1,11 @@
+# Enum
+Enum.__info__(:functions) |> Enum.each(fn({function, arity})-> 
+  IO.puts "#{function}/#{arity}"
+end) 
+
+# all?/2 todos membros devem ser avalida inteira como verdadeira
+Enum.all?(["eee", "fff", "etc"], fn(s) -> String.length(s) == 3 end)
+# se ao menos um passar a função any?/2 retorna true
+Enum.any?(["j", "afl"], fn(s) -> String.length(s) == 3 end)
+# Eliminar itens duplicados
+Enum.uniq_by([1, 2, 3, 2, 1, 1, 1, 1, 1], fn x -> x end)
