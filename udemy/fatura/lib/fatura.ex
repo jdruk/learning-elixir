19a2cd605108b2314aedaa3b8021dc40caa4ff8a
jdruk <devjosafa@gmail.com>
2019-08-19 12:26:21 -0300

    basico do mix:wq

---
+++ udemy/fatura/lib/fatura.ex
@@ -0,0 +1,34 @@
+defmodule Fatura do
+  @moduledoc """
+  Documentation for Fatura.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fatura.hello()
+      :world
+
+  """
+  def hello do 
+    :world
+  end 
+
+  def new  do
+   [ :world, 1, "josafa"]
+  end
+
+  @doc """
+  sorted list of the `faturas`.
+
+  ## Ex:
+      iex>Fatura.sorted [1, "josafa", :world]
+      [1, :world, "josafa" ]
+  """
+  def sorted faturas do 
+    Enum.sort faturas 
+  end
+
+end
