c4bc616f4bf8afee59a24bbe4c6bef825a29775f
jdruk <devjosafa@gmail.com>
2019-08-21 21:39:23 -0300

    modified:   test/fatura_test.exs

--- udemy/fatura/test/fatura_test.exs
+++ udemy/fatura/test/fatura_test.exs
@@ -5,4 +5,8 @@ defmodule FaturaTest do
   test "greets the world" do
     assert Fatura.hello() == :world
   end
+
+  test "exemplo refute" do 
+    refute Fatura.sorted(["teste", "aaaa", "bbb"]) == ["teste", "aaaa", "bbb"]
+  end
 end
